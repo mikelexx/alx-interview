ec6497bf70eb6a6f0bef9d4e125665ec8a4332fa
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-31 07:47:40 +0300

    use bfs instead of recursion to optimize space complexity

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
+import collections
 """contains interview question solution for Lockboxes """
-
-
-def unlock_box(boxes, idx, box_size, vis_boxes):
-    """unlocks all the boxes it can using the keys provided
-    at the box at idx position
-    """
-    for key in boxes[idx]:
-        if type(key) is int \
-         and key not in vis_boxes and key < box_size:
-            vis_boxes.add(key)
-            unlock_box(boxes, key, box_size, vis_boxes)
 
 
 def canUnlockAll(boxes):
@@ -18,8 +8,13 @@ def canUnlockAll(boxes):
     determines if all locked boxes can be
     unlocked using keys provied at zeroth box
     """
-    if not boxes or type(boxes) is not list:
-        return False
-    vis_boxes = set([0])
-    unlock_box(boxes, 0, len(boxes), vis_boxes)
-    return len(vis_boxes) == len(boxes)
+    queue = collections.deque(boxes[0])
+    vis = set([0])
+    box_size = len(boxes)
+    while len(queue):
+        key = queue.popleft()
+        if key not in vis and key < box_size:
+            for el in boxes[key]:
+                queue.append(el)
+            vis.add(key)
+    return len(vis) == box_size
